ff589b705a617c1840712b87df43a26f721a1eb4
Arun Sharma <arun@sharma-home.net>
2016-06-14 15:29:13 -0700

    autopep8

--- simpleRaft/tests/raft.py
+++ simpleRaft/tests/raft.py
@@ -15,84 +15,84 @@ from states.follower import Follower
 
 N = 5
 
-class TestRaft( unittest.TestCase ):
+
+class TestRaft(unittest.TestCase):
 
     @classmethod
-    def setUpClass( self ):
+    def setUpClass(self):
         self.servers = []
         for i in range(N):
             if i == 0:
                 state = Leader()
             else:
                 state = Follower()
-            s = ZeroMQServer("S%d" % i, state, [], MemoryBoard(), [], 6666+i)
+            s = ZeroMQServer("S%d" % i, state, [], MemoryBoard(), [], 6666 + i)
             if (i == 0):
                 self.leader = s
             self.servers.append(s)
         for i in range(N):
             me = self.servers[i]
-            neighbors = [ n for n in self.servers if n != me ]
+            neighbors = [n for n in self.servers if n != me]
             me.set_neighbors(neighbors)
 
     @classmethod
-    def tearDownClass( self ):
+    def tearDownClass(self):
         pass
 
-    def _perform_hearbeat( self ):
+    def _perform_hearbeat(self):
         self.leader._state._send_heart_beat()
         for i in self.leader._neighbors:
-            i.on_message( i._messageBoard.get_message() )
+            i.on_message(i._messageBoard.get_message())
 
         for i in self.leader._messageBoard._board:
-            self.leader.on_message( i )
+            self.leader.on_message(i)
 
-    def test_heartbeat( self ):
+    def test_heartbeat(self):
 
         self._perform_hearbeat()
         expected = dict(('S%d' % i, 0) for i in range(1, N))
-        self.assertEqual( expected, self.leader._state._nextIndexes )
+        self.assertEqual(expected, self.leader._state._nextIndexes)
 
-    def test_append( self ):
-
-        self._perform_hearbeat()
-
-        msg = AppendEntriesMessage( 0, None, 1, {
-                                                "prevLogIndex": 0,
-                                                "prevLogTerm": 0,
-                                                "leaderCommit": 1,
-                                                "entries": [ { "term": 1, "value": 100 } ] } )
-
-        self.leader.send_message( msg )
-
-        for i in self.leader._neighbors:
-            i.on_message( i._messageBoard.get_message() )
-
-        for i in self.leader._neighbors:
-            self.assertEqual( [{ "term": 1, "value": 100 } ], i._log )
-
-
-    def test_dirty( self ):
-
-        self.leader._neighbors[0]._log.append( { "term": 2, "value": 100 } )
-        self.leader._neighbors[0]._log.append( { "term": 2, "value": 200 } )
-        self.leader._neighbors[1]._log.append( { "term": 3, "value": 200 } )
-        self.leader._log.append( { "term": 1, "value": 100 } )
+    def test_append(self):
 
         self._perform_hearbeat()
 
-        msg = AppendEntriesMessage( 0, None, 1, {
-                                                "prevLogIndex": 0,
-                                                "prevLogTerm": 0,
-                                                "leaderCommit": 1,
-                                                "entries": [ { "term": 1, "value": 100 } ] } )
+        msg = AppendEntriesMessage(0, None, 1, {
+            "prevLogIndex": 0,
+            "prevLogTerm": 0,
+            "leaderCommit": 1,
+            "entries": [{"term": 1, "value": 100}]})
 
-        self.leader.send_message( msg )
-
-        for i in self.leader._neighbors:
-            i.on_message( i._messageBoard.get_message() )
+        self.leader.send_message(msg)
 
         for i in self.leader._neighbors:
-            self.assertEqual( [{ "term": 1, "value": 100 } ], i._log )
+            i.on_message(i._messageBoard.get_message())
+
+        for i in self.leader._neighbors:
+            self.assertEqual([{"term": 1, "value": 100}], i._log)
+
+    def test_dirty(self):
+
+        self.leader._neighbors[0]._log.append({"term": 2, "value": 100})
+        self.leader._neighbors[0]._log.append({"term": 2, "value": 200})
+        self.leader._neighbors[1]._log.append({"term": 3, "value": 200})
+        self.leader._log.append({"term": 1, "value": 100})
+
+        self._perform_hearbeat()
+
+        msg = AppendEntriesMessage(0, None, 1, {
+            "prevLogIndex": 0,
+            "prevLogTerm": 0,
+            "leaderCommit": 1,
+            "entries": [{"term": 1, "value": 100}]})
+
+        self.leader.send_message(msg)
+
+        for i in self.leader._neighbors:
+            i.on_message(i._messageBoard.get_message())
+
+        for i in self.leader._neighbors:
+            self.assertEqual([{"term": 1, "value": 100}], i._log)
 
 
 if __name__ == '__main__':
--- simpleRaft/tests/test_MemoryBoard.py
+++ simpleRaft/tests/test_MemoryBoard.py
@@ -6,27 +6,28 @@ sys.path.append('..')
 from boards.memory_board import MemoryBoard
 from messages.base import BaseMessage
 
-class TestMemoryBoard( unittest.TestCase ):
 
-    def setUp( self ):
+class TestMemoryBoard(unittest.TestCase):
+
+    def setUp(self):
         self.board = MemoryBoard()
 
-    def test_memoryboard_post_message( self ):
+    def test_memoryboard_post_message(self):
 
-        msg = BaseMessage( 0, 0, 0, 0 )
-        self.board.post_message( msg )
-        self.assertEquals( msg, self.board.get_message() )
+        msg = BaseMessage(0, 0, 0, 0)
+        self.board.post_message(msg)
+        self.assertEquals(msg, self.board.get_message())
 
-    def test_memoryboard_post_message_make_sure_they_are_ordered( self ):
+    def test_memoryboard_post_message_make_sure_they_are_ordered(self):
 
-        msg = BaseMessage( 0, 0, 0, 0 )
-        msg2 = BaseMessage( 0, 0, 0, 0 )
+        msg = BaseMessage(0, 0, 0, 0)
+        msg2 = BaseMessage(0, 0, 0, 0)
         msg2._timestamp -= 100
 
-        self.board.post_message( msg )
-        self.board.post_message( msg2 )
+        self.board.post_message(msg)
+        self.board.post_message(msg2)
 
-        self.assertEquals( msg2, self.board.get_message() )
+        self.assertEquals(msg2, self.board.get_message())
 
 if __name__ == '__main__':
     unittest.main()
