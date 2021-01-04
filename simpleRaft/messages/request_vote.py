29fbe9c2a2619f3c2e2a21d233d9da621564f965
Arun Sharma <arun@sharma-home.net>
2021-01-03 19:59:06 -0800

    Decorate other message types

--- simpleRaft/messages/request_vote.py
+++ simpleRaft/messages/request_vote.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from serde import deserialize, serialize
 
 from .base import BaseMessage
 
 
+@deserialize
+@serialize
 @dataclass
 class RequestVoteMessage(BaseMessage):
 
