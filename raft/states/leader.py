7af871b5c1162af6805d57df8344540191e54b89
Arun Sharma <arun@sharma-home.net>
2021-02-04 14:50:35 -0800

    learners: keep track of them in a separate array
    Compute an alternative commit index based on a different algorithm
    This assumes that the persistent state is materialized by the learners
    and the leaders only track the commands.

--- raft/states/leader.py
+++ raft/states/leader.py
@@ -1,13 +1,21 @@
 import asyncio
 import logging
+import math
+from raft.messages.response import ResponseMessage
 import statistics
 from collections import defaultdict
+from enum import IntFlag
 
 from ..messages.append_entries import AppendEntriesMessage, Command
-from .config import HEART_BEAT_INTERVAL, SEND_ENTRIES_INTERVAL
+from .config import HEART_BEAT_INTERVAL, SEND_ENTRIES_INTERVAL, COMMIT_MODE
 from .state import State
 
 logger = logging.getLogger("raft")
+
+
+class CommitMode(IntFlag):
+    RAFT = 1
+    QUORUM_WRITE = 2
 
 
 class Leader(State):
@@ -17,16 +25,17 @@ class Leader(State):
         def default_next_index() -> int:
             return self._server._lastLogIndex + 1
 
+        super().__init__(timeout=0)
         self._nextIndex = defaultdict(default_next_index)
         self._matchIndex = defaultdict(int)
+        self._learnerIndex = defaultdict(int)
+        self._commit_mode = COMMIT_MODE
         self.timer = None  # Used by followers/candidates for leader timeout
         if not self.TEST_ONLY_DISABLE_SEND_LOOP:
             asyncio.create_task(self.append_entries_loop())
 
     def __repr__(self):
-        return (
-            f"Leader:\n\tnextIndex: {self._nextIndex}\n\tmatchIndex: {self._matchIndex}"
-        )
+        return f"Leader:\n\tnextIndex: {self._nextIndex}\n\tmatchIndex: {self._matchIndex}\n\tlearnerIndex: {self._learnerIndex}"
 
     def set_server(self, server):
         self._server = server
@@ -45,6 +54,7 @@ class Leader(State):
                 n = n._name
             self._nextIndex[n] = self._server._lastLogIndex + 1
             self._matchIndex[n] = 0
+            self._learnerIndex[n] = 0
 
         return heart_beat_task
 
@@ -99,16 +109,45 @@ class Leader(State):
             return self, None
         else:
             if num_entries > 0 and original_message:
-                # The last append was good so increase their index.
-                self._matchIndex[message.sender] = (
-                    original_message.prev_log_index + num_entries
-                )
-                self._nextIndex[message.sender] = max(
-                    self._nextIndex[message.sender],
-                    self._matchIndex[message.sender] + 1,
-                )
-                logger.debug(f"Advanced {message.sender} by {num_entries}")
-                new_commit_index = statistics.median_low(self._matchIndex.values())
+                if message.role == ResponseMessage.Role.FOLLOWER:
+                    # The last append was good so increase their index.
+                    self._matchIndex[message.sender] = (
+                        original_message.prev_log_index + num_entries
+                    )
+                    self._nextIndex[message.sender] = max(
+                        self._nextIndex[message.sender],
+                        self._matchIndex[message.sender] + 1,
+                    )
+                    logger.debug(f"Advanced {message.sender} by {num_entries}")
+                elif message.role == ResponseMessage.Role.LEARNER:
+                    self._learnerIndex[message.sender] = (
+                        original_message.prev_log_index + num_entries
+                    )
+                    self._nextIndex[message.sender] = max(
+                        self._nextIndex[message.sender],
+                        self._learnerIndex[message.sender] + 1,
+                    )
+                    logger.debug(f"Learner: Advanced {message.sender} by {num_entries}")
+                new_commit_index_raft = new_commit_index_quorum = 0
+                if self._commit_mode & CommitMode.RAFT:
+                    new_commit_index_raft = statistics.median_low(
+                        self._matchIndex.values()
+                    )
+                if self._commit_mode & CommitMode.QUORUM_WRITE:
+                    values = [v for v in sorted(self._learnerIndex.values()) if v > 0]
+                    # compute 4 out of 6
+                    l = len(values)
+                    if l:
+                        m = math.ceil(l * 4 / 6)
+                        new_commit_index_quorum = values[-m] if m <= len(values) else 0
+                if self._commit_mode == CommitMode.RAFT:
+                    new_commit_index = new_commit_index_raft
+                elif self._commit_mode == CommitMode.QUORUM_WRITE:
+                    new_commit_index = new_commit_index_quorum
+                elif self._commit_mode == CommitMode.RAFT | CommitMode.QUORUM_WRITE:
+                    new_commit_index = min(
+                        new_commit_index_raft, new_commit_index_quorum
+                    )
                 if (
                     self._server._log[new_commit_index].term
                     == self._server._currentTerm
